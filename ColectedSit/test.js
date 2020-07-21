3d1df85e12e0d7a4a309c3caf740fc58ed80b949
meshall2019 <46848565+meshall2019@users.noreply.github.com>
2020-07-21 18:28:13 +0300

    Add files via upload

---
+++ ColectedSit/test.js
@@ -0,0 +1,17 @@
+function sum(){
+
+    var first=parseInt(document.querySelector(".first").value);
+    var second=parseInt(document.querySelector(".Second").value);
+     
+
+    var sum=first+second;
+    
+    alert(sum)
+
+    }
+
+
+
+
+
+
